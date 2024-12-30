4c206bdcfdc66e7c9058aa9d4063c61ac8e04df6
Selena Kunsman <148573789+smkunsman@users.noreply.github.com>
2024-12-30 03:55:02 +0000

    refactored Board

--- src/App.js
+++ src/App.js
@@ -31,24 +31,32 @@ function Board({ xIsNext, squares, onPlay }) {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
+  const boardSize = 3;
+  const rows = [];
+
+  for (let row = 0; row < boardSize; row++) {
+    const squaresRow = [];
+    for (let col = 0; col < boardSize; col++) {
+      const index = row * boardSize + col;
+      squaresRow.push(
+        <Square
+          key={index}
+          value={squares[index]}
+          onSquareClick={() => handleClick(index)}
+        />
+      );
+    }
+    rows.push(
+      <div key={row} className="board-row">
+        {squaresRow}
+      </div>
+    );
+  }
+
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {rows}
     </>
   );
 }
